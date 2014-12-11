d3842d573d2b03a4bc5551188458587bb440afb1
natriveras <narivera|@alumno.ubiobio.cl>
2014-12-11 01:34:18 -0300

    todo
    todo

--- RNSChat/src/com/example/rnschat/gcm/GcmBroadcastReceiver.java
+++ RNSChat/src/com/example/rnschat/gcm/GcmBroadcastReceiver.java
@@ -1,74 +1,82 @@
-/**
- * 
- */
+package com.example.rnschat.gcm;
 
-/**
- * @author Nalia
- *
- */
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
+import android.support.v4.app.NotificationCompat;
+import android.text.TextUtils;
+
+import com.example.rnschat.Common;
+import com.example.rnschat.DataProvider;
+import com.example.rnschat.DataProvider.MessageType;
+import com.example.rnschat.MainActivity;
+import com.example.rnschat.R;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
 public class GcmBroadcastReceiver extends BroadcastReceiver {
+	
+	private static final String TAG = "GcmBroadcastReceiver";
+	private Context ctx;	
 
-	 private static final String TAG = "GcmBroadcastReceiver";
-	    private Context ctx;    
-	 
-	    @Override
-	    public void onReceive(Context context, Intent intent) {
-	        ctx = context;
-	         
-	        PowerManager mPowerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
-	        WakeLock mWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
-	        mWakeLock.acquire();
-	         
-	        try {
-	            GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(context);
-	             
-	            String messageType = gcm.getMessageType(intent);
-	            if (GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
-	                sendNotification("Send error", false);
-	                 
-	            } else if (GoogleCloudMessaging.MESSAGE_TYPE_DELETED.equals(messageType)) {
-	                sendNotification("Deleted messages on server", false);
-	                 
-	            } else {
-	                String msg = intent.getStringExtra(DataProvider.COL_MSG);
-	                String email = intent.getStringExtra(DataProvider.COL_FROM);
-	                 
-	                ContentValues values = new ContentValues(2);
-	                values.put(DataProvider.COL_MSG, msg);
-	                values.put(DataProvider.COL_FROM, email);
-	                context.getContentResolver().insert(DataProvider.CONTENT_URI_MESSAGES, values);
-	                 
-	                if (Common.isNotify()) {
-	                    sendNotification("New message", true);
-	                }
-	            }
-	            setResultCode(Activity.RESULT_OK);
-	             
-	        } finally {
-	            mWakeLock.release();
-	        }
-	      
-	        private void sendNotification(String text, boolean launchApp) {
-	            NotificationManager mNotificationManager = (NotificationManager) ctx.getSystemService(Context.NOTIFICATION_SERVICE);
-	             
-	            Notification.Builder mBuilder = new Notification.Builder(ctx)
-	                .setAutoCancel(true)
-	                .setSmallIcon(R.drawable.ic_launcher)
-	                .setContentTitle(ctx.getString(R.string.app_name))
-	                .setContentText(text);
-	         
-	            if (!TextUtils.isEmpty(Common.getRingtone())) {
-	                mBuilder.setSound(Uri.parse(Common.getRingtone()));
-	            }
-	             
-	            if (launchApp) {
-	                Intent intent = new Intent(ctx, MainActivity.class);
-	                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-	                PendingIntent pi = PendingIntent.getActivity(ctx, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-	                mBuilder.setContentIntent(pi);
-	            }
-	             
-	            mNotificationManager.notify(1, mBuilder.getNotification());
-	        }
-	            
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		ctx = context;
+		PowerManager mPowerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+		WakeLock mWakeLock = mPowerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
+		mWakeLock.acquire();
+		try {
+			GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(context);
+			String messageType = gcm.getMessageType(intent);
+			if (GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
+				sendNotification("Send error", false);
+			} else if (GoogleCloudMessaging.MESSAGE_TYPE_DELETED.equals(messageType)) {
+				sendNotification("Deleted messages on server", false);
+			} else {
+				String msg = intent.getStringExtra(DataProvider.COL_MESSAGE);
+				String senderEmail = intent.getStringExtra(DataProvider.COL_SENDER_EMAIL);
+				String receiverEmail = intent.getStringExtra(DataProvider.COL_RECEIVER_EMAIL);
+				ContentValues values = new ContentValues(2);
+				values.put(DataProvider.COL_TYPE,  MessageType.INCOMING.ordinal());				
+				values.put(DataProvider.COL_MESSAGE, msg);
+				values.put(DataProvider.COL_SENDER_EMAIL, senderEmail);
+				values.put(DataProvider.COL_RECEIVER_EMAIL, receiverEmail);
+				context.getContentResolver().insert(DataProvider.CONTENT_URI_MESSAGES, values);
+				
+				if (Common.isNotify()) {
+					sendNotification("New message", true);
+				}
+			}
+			setResultCode(Activity.RESULT_OK);
+		} finally {
+			mWakeLock.release();
+		}
+	}
+
+	private void sendNotification(String text, boolean launchApp) {
+		NotificationManager mNotificationManager = (NotificationManager) ctx.getSystemService(Context.NOTIFICATION_SERVICE);
+		NotificationCompat.Builder notification = new NotificationCompat.Builder(ctx);
+		notification.setContentTitle(ctx.getString(R.string.app_name));
+		notification.setContentText(text);
+		notification.setAutoCancel(true);
+		notification.setSmallIcon(R.drawable.ic_launcher);
+		if (!TextUtils.isEmpty(Common.getRingtone())) {
+			notification.setSound(Uri.parse(Common.getRingtone()));
+		}
+		
+		if (launchApp) {
+			Intent intent = new Intent(ctx, MainActivity.class);
+			intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+			PendingIntent pi = PendingIntent.getActivity(ctx, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+			notification.setContentIntent(pi);
+		}
+	
+		mNotificationManager.notify(1, notification.build());
+	}
 }
--- RNSChat/src/com/example/rnschat/gcm/GcmUtil.java
+++ RNSChat/src/com/example/rnschat/gcm/GcmUtil.java
@@ -1,231 +1,187 @@
-package com.appsrox.instachat.gcm;
+package com.example.rnschat.gcm;
 
 import java.io.IOException;
 import java.util.Random;
 
-import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.AsyncTask;
-import android.text.TextUtils;
-import android.util.Log;
+import android.preference.PreferenceManager;
 
-import com.appsrox.instachat.Common;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.example.rnschat.Common;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 public class GcmUtil {
-
-    private static final String TAG = "GcmUtil";	
-
-    public static final String PROPERTY_REG_ID = "registration_id";
-    private static final String PROPERTY_APP_VERSION = "appVersion";
-    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+	
+	private static final String TAG = "GcmUtil";
+	
+	public static final String PROPERTY_REG_ID = "registration_id";
+	private static final String PROPERTY_APP_VERSION = "appVersion";
+	private static final String PROPERTY_ON_SERVER_EXPIRATION_TIME = "onServerExpirationTimeMs";
+	
+    /**
+     * Default lifespan (7 days) of a reservation until it is considered expired.
+     */
+    public static final long REGISTRATION_EXPIRY_TIME_MS = 1000 * 3600 * 24 * 7;
     
     private static final int MAX_ATTEMPTS = 5;
     private static final int BACKOFF_MILLI_SECONDS = 2000;
-    private static final Random random = new Random();
+    private static final Random random = new Random();    
 
-    private GoogleCloudMessaging gcm;
-    private Context context;
-    private String regid;
-    
-    private AsyncTask registrationTask;
-    
-    /**
-     * @param context
-     */
-    public GcmUtil(Context context) {
-    	this.context = context;
-    }
-    
-    public boolean register(Activity activity) {
-    	Log.e("register", "register");
-        // Check device for Play Services APK. If check succeeds, proceed with GCM registration.
-        if (checkPlayServices(activity)) {
-            gcm = GoogleCloudMessaging.getInstance(context);
-            regid = getRegistrationId(context);
+    private Context ctx;
+	private SharedPreferences prefs;
+	private GoogleCloudMessaging gcm;
+	private AsyncTask registrationTask;
 
-            if (TextUtils.isEmpty(regid)) {
-                if (registrationTask == null) 
-                	if (activity instanceof GcmListener) registerInBackground((GcmListener)activity);
-                	else registerInBackground(null);
-            } else {
-            	Log.i(TAG, regid);
-            	return true;
-            }
+	public GcmUtil(Context applicationContext) {
+		super();
+		ctx = applicationContext;
+		prefs = PreferenceManager.getDefaultSharedPreferences(ctx);
+		
+		String regid = getRegistrationId();
+		if (regid.length() == 0) {
+            registerBackground();
         } else {
-            Log.i(TAG, "No valid Google Play Services APK found.");
+        	broadcastStatus(true);
         }
-        return false;
-    }
+		gcm = GoogleCloudMessaging.getInstance(ctx);		
+	}
+	
+	/**
+	 * Gets the current registration id for application on GCM service.
+	 * <p>
+	 * If result is empty, the registration has failed.
+	 *
+	 * @return registration id, or empty string if the registration is not
+	 *         complete.
+	 */
+	private String getRegistrationId() {
+	    String registrationId = prefs.getString(PROPERTY_REG_ID, "");
+	    if (registrationId.length() == 0) {
+	        //Log.v(TAG, "Registration not found.");
+	        return "";
+	    }
+	    // check if app was updated; if so, it must clear registration id to
+	    // avoid a race condition if GCM sends a message
+	    int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
+	    int currentVersion = getAppVersion();
+	    if (registeredVersion != currentVersion || isRegistrationExpired()) {
+	        //Log.v(TAG, "App version changed or registration expired.");
+	        return "";
+	    }
+	    return registrationId;
+	}
+	
+	/**
+	 * Stores the registration id, app versionCode, and expiration time in the
+	 * application's {@code SharedPreferences}.
+	 *
+	 * @param regId registration id
+	 */
+	private void setRegistrationId(String regId) {
+	    int appVersion = getAppVersion();
+	    //Log.v(TAG, "Saving regId on app version " + appVersion);
+	    SharedPreferences.Editor editor = prefs.edit();
+	    editor.putString(PROPERTY_REG_ID, regId);
+	    editor.putInt(PROPERTY_APP_VERSION, appVersion);
+	    long expirationTime = System.currentTimeMillis() + REGISTRATION_EXPIRY_TIME_MS;
 
-    /**
-     * Check the device to make sure it has the Google Play Services APK. If
-     * it doesn't, display a dialog that allows users to download the APK from
-     * the Google Play Store or enable it in the device's system settings.
-     */
-    private boolean checkPlayServices(Activity activity) {
-        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(context);
-        if (resultCode != ConnectionResult.SUCCESS) {
-            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
-                GooglePlayServicesUtil.getErrorDialog(resultCode, activity, PLAY_SERVICES_RESOLUTION_REQUEST).show();
-            } else {
-                Log.i(TAG, "This device is not supported.");
-            }
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * Stores the registration ID and the app versionCode in the application's
-     * {@code SharedPreferences}.
-     *
-     * @param context application's context.
-     * @param regId registration ID
-     */
-    private void storeRegistrationId(Context context, String regId) {
-        final SharedPreferences prefs = getGcmPreferences(context);
-        int appVersion = getAppVersion(context);
-        Log.i(TAG, "Saving regId on app version " + appVersion);
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putString(PROPERTY_REG_ID, regId);
-        editor.putInt(PROPERTY_APP_VERSION, appVersion);
-        editor.commit();
-    }
-
-    /**
-     * Gets the current registration ID for application on GCM service, if there is one.
-     * <p>
-     * If result is empty, the app needs to register.
-     *
-     * @return registration ID, or empty string if there is no existing
-     *         registration ID.
-     */
-    private String getRegistrationId(Context context) {
-        final SharedPreferences prefs = getGcmPreferences(context);
-        String registrationId = prefs.getString(PROPERTY_REG_ID, "");
-        if (TextUtils.isEmpty(registrationId)) {
-            Log.i(TAG, "Registration not found.");
-            return "";
-        }
-        // Check if app was updated; if so, it must clear the registration ID
-        // since the existing regID is not guaranteed to work with the new
-        // app version.
-        int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
-        int currentVersion = getAppVersion(context);
-        if (registeredVersion != currentVersion) {
-            Log.i(TAG, "App version changed.");
-            return "";
-        }
-        return registrationId;
-    }
-
-    /**
-     * Registers the application with GCM servers asynchronously.
-     * <p>
-     * Stores the registration ID and the app versionCode in the application's
-     * shared preferences.
-     */
-    private void registerInBackground(final GcmListener listener) {
-    	registrationTask = new AsyncTask<Void, Void, Boolean>() {
-    		String msg = "";
-    		
-            @Override
-            protected Boolean doInBackground(Void... params) {
+	    //Log.v(TAG, "Setting registration expiry time to " + new Timestamp(expirationTime));
+	    editor.putLong(PROPERTY_ON_SERVER_EXPIRATION_TIME, expirationTime);
+	    editor.commit();
+	}	
+	
+	/**
+	 * @return Application's version code from the {@code PackageManager}.
+	 */
+	private int getAppVersion() {
+	    try {
+	        PackageInfo packageInfo = ctx.getPackageManager().getPackageInfo(ctx.getPackageName(), 0);
+	        return packageInfo.versionCode;
+	    } catch (NameNotFoundException e) {
+	        // should never happen
+	        throw new RuntimeException("Could not get package name: " + e);
+	    }
+	}
+	
+	/**
+	 * Checks if the registration has expired.
+	 *
+	 * <p>To avoid the scenario where the device sends the registration to the
+	 * server but the server loses it, the app developer may choose to re-register
+	 * after REGISTRATION_EXPIRY_TIME_MS.
+	 *
+	 * @return true if the registration has expired.
+	 */
+	private boolean isRegistrationExpired() {
+	    // checks if the information is not stale
+	    long expirationTime = prefs.getLong(PROPERTY_ON_SERVER_EXPIRATION_TIME, -1);
+	    return System.currentTimeMillis() > expirationTime;
+	}	
+	
+	/**
+	 * Registers the application with GCM servers asynchronously.
+	 * <p>
+	 * Stores the registration id, app versionCode, and expiration time in the 
+	 * application's shared preferences.
+	 */
+	private void registerBackground() {
+		registrationTask = new AsyncTask<Void, Void, Boolean>() {
+	        @Override
+	        protected Boolean doInBackground(Void... params) {
 	            long backoff = BACKOFF_MILLI_SECONDS + random.nextInt(1000);
 	            for (int i = 1; i <= MAX_ATTEMPTS; i++) {
-	            	if (isCancelled()) break; 
-	            	
-	            	Log.d(TAG, "Attempt #" + i + " to register");                
-	                try {
-	                    if (gcm == null) {
-	                        gcm = GoogleCloudMessaging.getInstance(context);
-	                    }
-	                    regid = gcm.register(Common.getSenderId());
-	                    msg = "Device registered, registration ID=" + regid;
+	            	//Log.d(TAG, "Attempt #" + i + " to register");
+		            try {
+		                if (gcm == null) {
+		                    gcm = GoogleCloudMessaging.getInstance(ctx);
+		                }
+		                String regid = gcm.register(Common.getSenderId());
 	
-	                    // You should send the registration ID to your server over HTTP, so it
-	                    // can use GCM/HTTP or CCS to send messages to your app.
-	                	String chatId = ServerUtilities.register(Common.getChatId(), regid);
-	                	if (!TextUtils.isEmpty(chatId)) {
-	                		Common.setChatId(chatId);
-	                		
-		                    // Persist the regID - no need to register again.
-		                    storeRegistrationId(context, regid);
-		                    return Boolean.TRUE;
-	                	}
-	                    
-	                } catch (IOException ex) {
-	                	Log.e("IOException", ex.getMessage());
-	                    msg = "Error :" + ex.getMessage();
-	                    // If there is an error, don't just keep trying to register.
-	                    // Require the user to click a button again, or perform
-	                    // exponential back-off.
+		                // You should send the registration ID to your server over HTTP,
+		                // so it can use GCM/HTTP or CCS to send messages to your app.
+		                ServerUtilities.register(Common.getPreferredEmail(), regid);
+	
+		                // Save the regid - no need to register again.
+		                setRegistrationId(regid);
+		                return Boolean.TRUE;
+		                
+		            } catch (IOException ex) {
+		                //Log.e(TAG, "Failed to register on attempt " + i + ":" + ex);
 		                if (i == MAX_ATTEMPTS) {
 		                    break;
 		                }
 		                try {
-		                    Log.d(TAG, "Sleeping for " + backoff + " ms before retry");
+		                    //Log.d(TAG, "Sleeping for " + backoff + " ms before retry");
 		                    Thread.sleep(backoff);
 		                } catch (InterruptedException e1) {
 		                    // Activity finished before we complete - exit.
-		                    Log.d(TAG, "Thread interrupted: abort remaining retries!");
+		                    //Log.d(TAG, "Thread interrupted: abort remaining retries!");
 		                    Thread.currentThread().interrupt();
 		                }
 		                // increase backoff exponentially
-		                backoff *= 2;	                    
-	                }
+		                backoff *= 2;		                
+		            }
 	            }
-                return Boolean.FALSE;
-            }
+	            return Boolean.FALSE;
+	        }
 
-            @Override
-			protected void onCancelled() {
-				super.onCancelled();
-                if (listener != null) {
-                	listener.onRegister(false);
-                }
-                registrationTask = null;
-			}
-
-			@Override
-            protected void onPostExecute(Boolean result) {
-                Log.i(TAG, msg + "\n");
-                if (listener != null) {
-                	listener.onRegister(result);
-                }
-                registrationTask = null;
-            }
-        }.execute(null, null, null);
-    }
-
-    /**
-     * @return Application's version code from the {@code PackageManager}.
-     */
-    private static int getAppVersion(Context context) {
-        try {
-            PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
-            return packageInfo.versionCode;
-        } catch (NameNotFoundException e) {
-            // should never happen
-            throw new RuntimeException("Could not get package name: " + e);
-        }
-    }
-
-    /**
-     * @return Application's {@code SharedPreferences}.
-     */
-    private SharedPreferences getGcmPreferences(Context context) {
-        // This sample app persists the registration ID in shared preferences, but
-        // how you store the regID in your app is up to you.
-        return context.getSharedPreferences(context.getPackageName()+".GCM", Context.MODE_PRIVATE);
-    }    
-    
+	        @Override
+	        protected void onPostExecute(Boolean status) {
+	        	broadcastStatus(status);
+	        }
+	    }.execute(null, null, null);
+	}
+	
+	private void broadcastStatus(boolean status) {
+    	Intent intent = new Intent(Common.ACTION_REGISTER);
+        intent.putExtra(Common.EXTRA_STATUS, status ? Common.STATUS_SUCCESS : Common.STATUS_FAILED);
+        ctx.sendBroadcast(intent);		
+	}
+	
 	public void cleanup() {
 		if (registrationTask != null) {
 			registrationTask.cancel(true);
@@ -233,5 +189,6 @@ public class GcmUtil {
 		if (gcm != null) {
 			gcm.close();
 		}
-	}    
+	}	
+	
 }
