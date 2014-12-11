ce9c685df266a9a92a92de1108fe9f63d4739b33
natriveras <narivera|@alumno.ubiobio.cl>
2014-12-11 00:30:28 -0300

    10

--- RNSChat/src/com/example/rnschat/DataProvider.java
+++ RNSChat/src/com/example/rnschat/DataProvider.java
@@ -30,7 +30,28 @@ public class DataProvider extends ContentProvider{
  
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-        return null;
+    	SQLiteDatabase db = dbHelper.getReadableDatabase();
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+         
+        switch(uriMatcher.match(uri)) {
+        case MESSAGES_ALLROWS:
+        case PROFILE_ALLROWS:
+            qb.setTables(getTableName(uri));
+            break;
+             
+        case MESSAGES_SINGLE_ROW:
+        case PROFILE_SINGLE_ROW:
+            qb.setTables(getTableName(uri));
+            qb.appendWhere("_id = " + uri.getLastPathSegment());
+            break;
+             
+        default:
+            throw new IllegalArgumentException("Unsupported URI: " + uri);          
+        }
+         
+        Cursor c = qb.query(db, projection, selection, selectionArgs, null, null, sortOrder);
+        c.setNotificationUri(getContext().getContentResolver(), uri);
+        return c;
     }
  
     @Override
@@ -40,16 +61,230 @@ public class DataProvider extends ContentProvider{
  
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        return null;
+    	SQLiteDatabase db = dbHelper.getWritableDatabase();
+        
+        long id;
+        switch(uriMatcher.match(uri)) {
+        case MESSAGES_ALLROWS:
+            id = db.insertOrThrow(TABLE_MESSAGES, null, values);
+            if (values.get(COL_TO) == null) {
+                db.execSQL("update profile set count=count+1 where email = ?", new Object[]{values.get(COL_FROM)});
+                getContext().getContentResolver().notifyChange(CONTENT_URI_PROFILE, null);
+            }
+            break;
+             
+        case PROFILE_ALLROWS:
+            id = db.insertOrThrow(TABLE_PROFILE, null, values);
+            break;
+             
+        default:
+            throw new IllegalArgumentException("Unsupported URI: " + uri);
+        }
+         
+        Uri insertUri = ContentUris.withAppendedId(uri, id);
+        getContext().getContentResolver().notifyChange(insertUri, null);
+        return insertUri;
     }
  
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        return 0;
+    	SQLiteDatabase db = dbHelper.getWritableDatabase();
+        
+        int count;
+        switch(uriMatcher.match(uri)) {
+        case MESSAGES_ALLROWS:
+        case PROFILE_ALLROWS:
+            count = db.delete(getTableName(uri), selection, selectionArgs);
+            break;
+             
+        case MESSAGES_SINGLE_ROW:
+        case PROFILE_SINGLE_ROW:
+            count = db.delete(getTableName(uri), "_id = ?", new String[]{uri.getLastPathSegment()});
+            break;
+             
+        default:
+            throw new IllegalArgumentException("Unsupported URI: " + uri);          
+        }
+         
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
     }
  
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        return 0;
+    	 SQLiteDatabase db = dbHelper.getWritableDatabase();
+         
+    	    int count;
+    	    switch(uriMatcher.match(uri)) {
+    	    case MESSAGES_ALLROWS:
+    	    case PROFILE_ALLROWS:
+    	        count = db.update(getTableName(uri), values, selection, selectionArgs);
+    	        break;
+    	         
+    	    case MESSAGES_SINGLE_ROW:
+    	    case PROFILE_SINGLE_ROW:
+    	        count = db.update(getTableName(uri), values, "_id = ?", new String[]{uri.getLastPathSegment()});
+    	        break;
+    	         
+    	    default:
+    	        throw new IllegalArgumentException("Unsupported URI: " + uri);          
+    	    }
+    	     
+    	    getContext().getContentResolver().notifyChange(uri, null);
+    	    return count;
     }
+    
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+         
+        switch(uriMatcher.match(uri)) {
+        case MESSAGES_ALLROWS:
+        case PROFILE_ALLROWS:
+            qb.setTables(getTableName(uri));
+            break;
+             
+        case MESSAGES_SINGLE_ROW:
+        case PROFILE_SINGLE_ROW:
+            qb.setTables(getTableName(uri));
+            qb.appendWhere("_id = " + uri.getLastPathSegment());
+            break;
+             
+        default:
+            throw new IllegalArgumentException("Unsupported URI: " + uri);          
+        }
+         
+        Cursor c = qb.query(db, projection, selection, selectionArgs, null, null, sortOrder);
+        c.setNotificationUri(getContext().getContentResolver(), uri);
+        return c;
+    }
+     
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+         
+        long id;
+        switch(uriMatcher.match(uri)) {
+        case MESSAGES_ALLROWS:
+            id = db.insertOrThrow(TABLE_MESSAGES, null, values);
+            if (values.get(COL_TO) == null) {
+                db.execSQL("update profile set count=count+1 where email = ?", new Object[]{values.get(COL_FROM)});
+                getContext().getContentResolver().notifyChange(CONTENT_URI_PROFILE, null);
+            }
+            break;
+             
+        case PROFILE_ALLROWS:
+            id = db.insertOrThrow(TABLE_PROFILE, null, values);
+            break;
+             
+        default:
+            throw new IllegalArgumentException("Unsupported URI: " + uri);
+        }
+         
+        Uri insertUri = ContentUris.withAppendedId(uri, id);
+        getContext().getContentResolver().notifyChange(insertUri, null);
+        return insertUri;
+    }
+     
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+         
+        int count;
+        switch(uriMatcher.match(uri)) {
+        case MESSAGES_ALLROWS:
+        case PROFILE_ALLROWS:
+            count = db.update(getTableName(uri), values, selection, selectionArgs);
+            break;
+             
+        case MESSAGES_SINGLE_ROW:
+        case PROFILE_SINGLE_ROW:
+            count = db.update(getTableName(uri), values, "_id = ?", new String[]{uri.getLastPathSegment()});
+            break;
+             
+        default:
+            throw new IllegalArgumentException("Unsupported URI: " + uri);          
+        }
+         
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
+     
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+         
+        int count;
+        switch(uriMatcher.match(uri)) {
+        case MESSAGES_ALLROWS:
+        case PROFILE_ALLROWS:
+            count = db.delete(getTableName(uri), selection, selectionArgs);
+            break;
+             
+        case MESSAGES_SINGLE_ROW:
+        case PROFILE_SINGLE_ROW:
+            count = db.delete(getTableName(uri), "_id = ?", new String[]{uri.getLastPathSegment()});
+            break;
+             
+        default:
+            throw new IllegalArgumentException("Unsupported URI: " + uri);          
+        }
+         
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
+     
+    private String getTableName(Uri uri) {
+        switch(uriMatcher.match(uri)) {
+        case MESSAGES_ALLROWS:
+        case MESSAGES_SINGLE_ROW:
+            return TABLE_MESSAGES;
+             
+        case PROFILE_ALLROWS:
+        case PROFILE_SINGLE_ROW:
+            return TABLE_PROFILE;           
+        }
+        return null;
+    }
+                    
+    
+    private static class DbHelper extends SQLiteOpenHelper {
+        
+        private static final String DATABASE_NAME = "rnschat.db";
+        private static final int DATABASE_VERSION = 1;
+     
+        public DbHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+     
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("create table messages (_id integer primary key autoincrement, msg text, email text, email2 text, at datetime default current_timestamp);");
+            db.execSQL("create table profile (_id integer primary key autoincrement, name text, email text unique, count integer default 0);");
+        }
+     
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        }
+    }
+            
+    public static final Uri CONTENT_URI_MESSAGES = Uri.parse("content://com.example.rnschat.provider/messages");
+    public static final Uri CONTENT_URI_PROFILE = Uri.parse("content://com.example.rnschat.provider/profile");
+     
+    private static final int MESSAGES_ALLROWS = 1;
+    private static final int MESSAGES_SINGLE_ROW = 2;
+    private static final int PROFILE_ALLROWS = 3;
+    private static final int PROFILE_SINGLE_ROW = 4;
+     
+    private static final UriMatcher uriMatcher;
+    static {
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI("com.example.rnschat.provider", "messages", MESSAGES_ALLROWS);
+        uriMatcher.addURI("com.example.rnschat.provider", "messages/#", MESSAGES_SINGLE_ROW);
+        uriMatcher.addURI("com.example.rnschat.provider", "profile", PROFILE_ALLROWS);
+        uriMatcher.addURI("com.example.rnschat.provider", "profile/#", PROFILE_SINGLE_ROW);
+    }
+          
+    
 }
